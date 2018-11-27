a0ffe8a616edf3887f40ca4a9ceda4600df3c902
Sean McPherson <seanmcp25@gmail.com>
2018-11-27 09:35:47 -0500

    Add filter for published posts

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -51,6 +51,7 @@ export const pageQuery = graphql`
       }
     }
     allMarkdownRemark(
+      filter: { frontmatter: { published: { eq: true } } },
       sort: { fields: [frontmatter___date], order: DESC }
     ) {
       edges {
